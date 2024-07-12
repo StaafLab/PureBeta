bf0a49b202ea103851a2725491d35fb421c57e54
ISasiain <inakisasiainca@gmail.com>
2024-07-12 23:22:13 +0200

    Update parallelization

--- R/reference_based_beta_correction.R
+++ R/reference_based_beta_correction.R
@@ -298,7 +298,7 @@ reference_based_beta_correction <- function(
   } else {
 
     registerDoSNOW(cl)
-    clusterExport(cl, list("PureBeta:::identify_regression", "PureBeta:::correcting_betas"), envir=NULL)
+    clusterExport(cl, list("PureBeta:::identify_regression", "PureBeta:::correcting_betas"), envir=environment())
 
     
     # PROCESSING PREDICTED PURITIES
