6f5524fb7caa91fded59f9f22e27e4730b61b5de
ISasiain <inakisasiainca@gmail.com>
2024-07-13 12:40:56 +0200

    Update script. Remove error

--- R/reference_based_beta_correction.R
+++ R/reference_based_beta_correction.R
@@ -416,6 +416,12 @@ reference_based_beta_correction <- function(
       )
     )
 
+    }
+
+    # Stop clusters used in parallelization
+    stopCluster(cl)
+
+
     # PROCESSING OUTPUT
 
     cat("\n\nGenerating output...\n\n")
@@ -435,19 +441,11 @@ reference_based_beta_correction <- function(
     cat ("PROCESS FINISHED")
     cat("\n=================\n\n")
 
-
-    # Stop clusters used in parallelization
-    stopCluster(cl)
-
     return(
       list(
         "Corrected_tumour" = corrected_tumor,
         "Corrected_microenvironment"  = corrected_microenvironment
       )
     )
-
-
-  }
-
 
 }
