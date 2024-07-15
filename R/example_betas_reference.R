a499a9d85ba9bb451eaa6965fa2acd7eb6928225
ISasiain <inakisasiainca@gmail.com>
2024-07-15 10:52:40 +0200

    Eliminating files not related to documentation

--- R/example_betas_reference.R
+++
@@ -1,8 +0,0 @@
-#' @title example_betas_reference
-#'
-#' @description A matrix containing the uncorrected beta values of the 5.000 most variable CpGs from the TCGA-BRCA dataset. This matrix is intended to be used as example reference data for the PureBeta package.
-#'
-#' @format A matrix with 5000 rows (CpGs) and 126 columns (samples).
-#'
-#' @source <https://github.com/StaafLab/PureBeta>
-"example_betas_reference"
--- R/example_purities_reference.R
+++
@@ -1,8 +0,0 @@
-#' @title example_purities_reference
-#'
-#' @description A named vector with the purity values of the samples included in the reference beta value matrix. It is intended to be used as a reference to correct betas or estimate purity based on reference regressions.
-#'
-#' @format A named vector with 126 elements.
-#'
-#' @source <https://github.com/StaafLab/PureBeta>
-"example_purities_reference"
