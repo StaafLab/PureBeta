c3ac92f709eb35743280b0b962493064e6ca1fcd
Iñaki Sasiain Casado <105723320+ISasiain@users.noreply.github.com>
2025-03-27 16:51:53 +0100

    Update utils.R. Update adjust betas function to deal with NA values

--- R/utils.R
+++ R/utils.R
@@ -7,186 +7,220 @@
 #
 
 adjustBeta <- function(
-
-    methylation,
-    purity,
-    snames,
-    nmax = 3,
-    nrep = 3,
-    seed = TRUE
-
-  ) {
-
-    #If a seed is provided as the first element of the methylation vector (seed=TRUE)
-    #set seed for the cluster determination and remove it from the methylation vector
-    if(seed) {
-      set.seed(as.integer(methylation[1]))
-      methylation<-methylation[-1]
-    }
-
-    #Defining variables for clustering and regression
-    x <- as.numeric(purity)
-    x2 <- 1-as.numeric(purity)
-    y <- as.numeric(methylation)
-
-    #Calculate global correlation between beta and purity
-    gl.corr <- suppressWarnings(cor(x,y))
-    gl.corr[is.na(gl.corr)] <- 0
-    gl.corr <- round(gl.corr,3)
-
-    #Add small gaussian noise to x (avoid errors when large number of zero samples)
-    y2<-y+rnorm(length(y),mean=0,sd=.005)
-
-    #Modeling the methylation patterns (CpG populations)
-    model <- stepFlexmix(y2 ~ x,k = 1:nmax, nrep = nrep,verbose = FALSE)
-    model <- getModel(model, "BIC")
-    cl <- clusters(model)
     
-    #Make sure clusters are numbered 1 to 3, odd cases exist where one pop has zero members from flexMix
-    #can rename because flexmix object not used after this
-    cl <- as.integer(factor(cl))
-
-
-
-    ## CALCULATING REGRESSIONS
-
-    #Determining the b_vs_pur regressions for the clusters identified
-    b_vs_pur <- lapply(1:nmax,function(z) {
-      if(z %in% cl) {
-        lm(y[cl==z]~x[cl==z]) #Beta VS 1-Purity regression
-
-      } else { NA } #If less than 3 populations were detected NA is added
-
-    })
-
-    #Determining the b_vs_1mp regressions for the clusters identified
-    b_vs_1mp <- lapply(1:nmax,function(z) {
-      if(z %in% cl) {
-        lm(y[cl==z]~x2[cl==z]) #Beta VS 1-Purity regression
-      } else { NA } #If less than 3 populations were detected NA is added
-    })
-
-
-    ## DETERMINING THE FUNCTION'S OUTPUT FROM THE CALCULATED REGRESSIONS
-
-    output_list <- list() #Creating a list to store the data
-
-    #Adding the population to which each CpG belongs to the list
-    output_list$groups <- cl
-
-    #Adding the number of population to which each CpG belongs to the list
-    output_list$n.groups <- length(levels(factor(cl)))
-
-    #Adding the correlation between betas and purity to the list
-    output_list$glob.corr <- gl.corr
-
-    #Adding corrected microenvironment betas to the list
-    output_list$y.norm <- sapply(
-      X = 1:nmax,
-      FUN = function(z) {
-        if (z %in% cl) {
-          n_vals <- coefficients(b_vs_pur[[z]])[1]+residuals(b_vs_pur[[z]])
-          names(n_vals)<-snames[cl==z]
-          n_vals
-
-        } else {
-          NULL
-        }
-      })
-    output_list$y.norm <- unlist(output_list$y.norm)[snames]
-
-    #Adding corrected tumor betas to the list
-    output_list$y.tum <- sapply(
-      X = 1:nmax,
-      FUN = function(z) {
-        if (z %in% cl) {
-          n_vals <- coefficients(b_vs_1mp[[z]])[1]+residuals(b_vs_1mp[[z]])
-          names(n_vals)<-snames[cl==z]
-          n_vals
-
-        } else {
-          NULL
-        }
-      })
-    output_list$y.tum <- unlist(output_list$y.tum)[snames]
-
-
-    ##in very rare instances flexmix calls 3 populations but one groups has zero members. -> has parameters for non-existant pop in output?!
-    #output_list$res.int <- round(as.numeric(unlist(lapply(slot(model,"components"),function(z) slot(z[[1]],"parameters")$coef[1]))),3)
-    #output_list$res.slopes <- round(as.numeric(unlist(lapply(slot(model,"components"),function(z) slot(z[[1]],"parameters")$coef[2]))),3)
-
-    #Getting line parameters from b_vs_1mp: INTERCEPTS
-    output_list$res.int <- sapply(
-      X = 1:nmax,
-      FUN = function(z) {
-        if (z %in% cl) {
-          int <- coefficients(b_vs_1mp[[z]])[1]
-          round(as.numeric(int),3)
-
-        } else {
-          NA
-        }
-      },
-      simplify = TRUE)
-
-    #Getting line parameters from b_vs_1mp: SLOPES
-    output_list$res.slopes <- sapply(
-      X = 1:nmax,
-      FUN = function(z) {
-        if (z %in% cl) {
-          int <- coefficients(b_vs_1mp[[z]])[2]
-          round(as.numeric(int),3)
-
-        } else {
-          NA
-        }
-      },
-      simplify = TRUE)
-
-
-    #Getting line parameters from b_vs_1mp: RESIDUAL STANDARD ERROR
-    output_list$res.rse <- sapply(
-      X = 1:nmax,
-      FUN = function(z) {
-        if (z %in% cl) {
-          RSE <- summary(b_vs_1mp[[z]])$sigma
-          round(as.numeric(RSE),6)
-
-        } else {
-          NA
-        }
-      },
-      simplify = TRUE)
-
-    #Getting line parameters from b_vs_1mp: RESIDUAL STANDARD ERROR
-    output_list$res.df <- sapply(
-      X = 1:nmax,
-      FUN = function(z) {
-        if (z %in% cl) {
-          df.residual(b_vs_1mp[[z]])
-
-        } else {
-          NA
-        }
-      },
-      simplify = TRUE)
-
-    #Cap the corrected betas to 0 and 1
-    output_list$y.tum[output_list$y.tum > 1] <- 1
-    output_list$y.tum[output_list$y.tum < 0] <- 0
-    output_list$y.norm[output_list$y.norm > 1] <- 1
-    output_list$y.norm[output_list$y.norm < 0] <- 0
-
-    #Round the betas to three decimal places
-    output_list$y.tum <- round(output_list$y.tum,3)
-    output_list$y.norm <- round(output_list$y.norm,3)
-
-    #Adding the rounded original betas to the list
-    output_list$y.orig <- round(methylation,3)
-
-    return(output_list)
-
-  }
+             methylation,
+             purity,
+             snames,
+             nmax = 3,
+             nrep = 3,
+             seed = TRUE
+             
+           ) {
+             
+             #If a seed is provided as the first element of the methylation vector (seed=TRUE)
+             #set seed for the cluster determination and remove it from the methylation vector
+             if(seed) {
+               set.seed(as.integer(methylation[1]))
+               methylation<-methylation[-1]
+             }
+             
+             #Dealing with NAs.
+  
+             # Getting indices
+             to_keep <- !is.na(methylation) & !is.na(purity)
+             
+             # Filtering data. Keep original betas for the output
+             original_methylation <- methylation
+             methylation <- methylation[to_keep]
+             purity <- purity[to_keep]
+             
+             # Filtering data. Keep original sample names for the output
+             original_names <- snames
+             snames <- snames[to_keep]
+             
+             # Indices of NA values
+             indices_of_NAs <- which(!to_keep)
+  
+             #Defining variables for clustering and regression
+             x <- as.numeric(purity)
+             x2 <- 1-as.numeric(purity)
+             y <- as.numeric(methylation)
+             
+             #Calculate global correlation between beta and purity
+             gl.corr <- suppressWarnings(cor(x,y))
+             gl.corr[is.na(gl.corr)] <- 0
+             gl.corr <- round(gl.corr,3)
+             
+             #Add small gaussian noise to x (avoid errors when large number of zero samples)
+             y2<-y+rnorm(length(y),mean=0,sd=.005)
+             
+             #Modeling the methylation patterns (CpG populations)
+             model <- stepFlexmix(y2 ~ x,k = 1:nmax, nrep = nrep,verbose = FALSE)
+             model <- getModel(model, "BIC")
+             cl <- clusters(model)
+             
+             #Make sure clusters are numbered 1 to 3, odd cases exist where one pop has zero members from flexMix
+             #can rename because flexmix object not used after this
+             cl <- as.integer(factor(cl))
+             
+             
+             
+             ## CALCULATING REGRESSIONS
+             
+             #Determining the b_vs_pur regressions for the clusters identified
+             b_vs_pur <- lapply(1:nmax,function(z) {
+               if(z %in% cl) {
+                 lm(y[cl==z]~x[cl==z]) #Beta VS 1-Purity regression
+                 
+               } else { NA } #If less than 3 populations were detected NA is added
+               
+             })
+             
+             #Determining the b_vs_1mp regressions for the clusters identified
+             b_vs_1mp <- lapply(1:nmax,function(z) {
+               if(z %in% cl) {
+                 lm(y[cl==z]~x2[cl==z]) #Beta VS 1-Purity regression
+               } else { NA } #If less than 3 populations were detected NA is added
+             })
+             
+             
+             ## DETERMINING THE FUNCTION'S OUTPUT FROM THE CALCULATED REGRESSIONS
+             
+             output_list <- list() #Creating a list to store the data
+             
+             #Adding the population to which each CpG belongs to the list
+             output_list$groups <- cl
+             
+             #Adding the number of population to which each CpG belongs to the list
+             output_list$n.groups <- length(levels(factor(cl)))
+             
+             #Adding the correlation between betas and purity to the list
+             output_list$glob.corr <- gl.corr
+             
+             #Adding corrected microenvironment betas to the list
+             output_list$y.norm <- sapply(
+               X = 1:nmax,
+               FUN = function(z) {
+                 if (z %in% cl) {
+                   n_vals <- coefficients(b_vs_pur[[z]])[1]+residuals(b_vs_pur[[z]])
+                   names(n_vals)<-snames[cl==z]
+                   n_vals
+                   
+                 } else {
+                   NULL
+                 }
+               })
+             output_list$y.norm <- unlist(output_list$y.norm)[snames]
+             
+             #Adding corrected tumor betas to the list
+             output_list$y.tum <- sapply(
+               X = 1:nmax,
+               FUN = function(z) {
+                 if (z %in% cl) {
+                   n_vals <- coefficients(b_vs_1mp[[z]])[1]+residuals(b_vs_1mp[[z]])
+                   names(n_vals)<-snames[cl==z]
+                   n_vals
+                   
+                 } else {
+                   NULL
+                 }
+               })
+             output_list$y.tum <- unlist(output_list$y.tum)[snames]
+             
+             
+             ##in very rare instances flexmix calls 3 populations but one groups has zero members. -> has parameters for non-existant pop in output?!
+             #output_list$res.int <- round(as.numeric(unlist(lapply(slot(model,"components"),function(z) slot(z[[1]],"parameters")$coef[1]))),3)
+             #output_list$res.slopes <- round(as.numeric(unlist(lapply(slot(model,"components"),function(z) slot(z[[1]],"parameters")$coef[2]))),3)
+             
+             #Getting line parameters from b_vs_1mp: INTERCEPTS
+             output_list$res.int <- sapply(
+               X = 1:nmax,
+               FUN = function(z) {
+                 if (z %in% cl) {
+                   int <- coefficients(b_vs_1mp[[z]])[1]
+                   round(as.numeric(int),3)
+                   
+                 } else {
+                   NA
+                 }
+               },
+               simplify = TRUE)
+             
+             #Getting line parameters from b_vs_1mp: SLOPES
+             output_list$res.slopes <- sapply(
+               X = 1:nmax,
+               FUN = function(z) {
+                 if (z %in% cl) {
+                   int <- coefficients(b_vs_1mp[[z]])[2]
+                   round(as.numeric(int),3)
+                   
+                 } else {
+                   NA
+                 }
+               },
+               simplify = TRUE)
+             
+             
+             #Getting line parameters from b_vs_1mp: RESIDUAL STANDARD ERROR
+             output_list$res.rse <- sapply(
+               X = 1:nmax,
+               FUN = function(z) {
+                 if (z %in% cl) {
+                   RSE <- summary(b_vs_1mp[[z]])$sigma
+                   round(as.numeric(RSE),6)
+                   
+                 } else {
+                   NA
+                 }
+               },
+               simplify = TRUE)
+             
+             #Getting line parameters from b_vs_1mp: RESIDUAL STANDARD ERROR
+             output_list$res.df <- sapply(
+               X = 1:nmax,
+               FUN = function(z) {
+                 if (z %in% cl) {
+                   df.residual(b_vs_1mp[[z]])
+                   
+                 } else {
+                   NA
+                 }
+               },
+               simplify = TRUE)
+             
+             #Cap the corrected betas to 0 and 1
+             output_list$y.tum[output_list$y.tum > 1] <- 1
+             output_list$y.tum[output_list$y.tum < 0] <- 0
+             output_list$y.norm[output_list$y.norm > 1] <- 1
+             output_list$y.norm[output_list$y.norm < 0] <- 0
+             
+             #Round the betas to three decimal places
+             output_list$y.tum <- round(output_list$y.tum,3)
+             output_list$y.norm <- round(output_list$y.norm,3)
+             
+             #Adding the rounded original betas to the list
+             output_list$y.orig <- round(original_methylation,3)
+             
+             # Adding back NA values (if any)
+             if (length(indices_of_NAs) > 0) {
+               
+               # Create full-length vectors with NA
+               full_y_tum <- rep(NA, length(original_names))
+               full_y_norm <- rep(NA, length(original_names))
+               
+               # Fill non-NA positions with computed values
+               full_y_tum[to_keep] <- output_list$y.tum
+               full_y_norm[to_keep] <- output_list$y.norm
+               
+               # Assign back to output list
+               output_list$y.tum <- setNames(full_y_tum, original_names)
+               output_list$y.norm <- setNames(full_y_norm, original_names)
+             }
+             
+             
+             return(output_list)
+             
+}
 
 
 #
